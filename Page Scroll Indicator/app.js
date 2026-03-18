d38fa09379386928864a5c24bae2d41e553b3378
AditProg <your-email@example.com>
2026-03-18 17:58:57 +0530

    Page Scrollbar

---
+++ Page Scroll Indicator/app.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll", () => {
+  const progress = document.querySelector(".progress");
+
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+
+  const scrollPercent = (scrollTop / docHeight) * 100;
+
+  progress.style.width = scrollPercent + "%";
+});
