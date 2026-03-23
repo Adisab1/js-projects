0e7d3804371c5ed4a913b73b63ce49219ce42486
AditProg <your-email@example.com>
2026-03-23 15:28:50 +0530

    Animated NAvigation

---
+++ 20.Animated Navigation/script.js
@@ -0,0 +1,6 @@
+const nav1 = document.getElementById("nav");
+const toggler = document.getElementById("toggle");
+
+toggler.addEventListener("click", () => {
+  nav1.classList.toggle("active");
+});
