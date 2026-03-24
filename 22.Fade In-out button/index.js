6a1ff82d3158c32b1b4f10a89109957a2d743afc
AditProg <your-email@example.com>
2026-03-24 16:15:07 +0530

    Fadein/out button

---
+++ 22.Fade In-out button/index.js
@@ -0,0 +1,10 @@
+const arrow = document.querySelector(".arrow");
+
+window.addEventListener("scroll", () => {
+  let position = window.scrollY;
+
+  if (position <= 5) {
+    arrow.classList.toggle("fade-in");
+    arrow.classList.toggle("fade-out");
+  }
+});
